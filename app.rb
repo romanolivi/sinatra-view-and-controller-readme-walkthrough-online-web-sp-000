a3bd001145451cd252c025bdbb02b58105c6de81
Romano Livi <romano_livi@outlook.com>
2021-01-26 22:43:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
 
   post '/reverse' do
     orginal_string = params["string"]
-    reversed_string = orginal_string.reverse
+    @reversed_string = orginal_string.reverse
     erb :reversed
   end
 
